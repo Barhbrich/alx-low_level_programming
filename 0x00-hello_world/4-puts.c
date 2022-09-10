6acfc99ce73c386f616c0421bd6c81cbfe271764
barhbrich <davidtjbarhbrich@gmail.com>
2022-09-09 17:33:12 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- printing c
+ *
+ * Return: Always 0 (Green)
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
