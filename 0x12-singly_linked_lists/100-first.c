6ba721c4fd4daec0941fbfdff2c515772c338362
barhbrich <davidtjbarhbrich@gmail.com>
2022-10-21 20:45:31 -0700

    advance tasks

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * before_main - prints statement before running main func
+ */
+void __attribute__((constructor)) before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+
